acca6488296a661860473ef257dfe0a82efbe5e5
Nahuel Prieto <nahuel.prieto@kaplan.edu>
2021-08-12 18:48:54 -0300

    agregada demo de arrays

---
+++ js/arrays.js
@@ -0,0 +1,17 @@
+// var mentores = ['Jamel', 'Nico', 'Nahue'];
+
+// for(var i = 0; i < 3; i++)
+// {
+//     alert(mentores[i]);
+// }
+
+var dias = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
+
+// for(var i = 6; i >= 0; i = i - 3 )
+// {
+//     alert('Tenés un partido el día ' + días[i]);
+// }
+
+for (var i in dias) {
+    alert(dias[i]);
+}
