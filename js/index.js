efd949f0cd268932de2d8efeda96c32823d8248f
nicolaspoy <nicolas.poy.contractor@macmillan.com>
2021-08-12 18:22:37 -0300

    Se agrega ejemplo de condicionales en javascript

--- js/index.js
+++ js/index.js
@@ -7,11 +7,34 @@
 //     alert("incorrecto");
 // }
 
-var showWelcomeMessage = true;
-var welcomeMessage = "Welcome to my website!";
+// var showWelcomeMessage = true;
+// var welcomeMessage = "Welcome to my website!";
 
-if (showWelcomeMessage === true) {
-    alert(welcomeMessage);
-} else {
-    console.log("testing");
+// if (showWelcomeMessage) {
+//     alert(welcomeMessage);
+// } else {
+//     console.log("testing");
+// }
+
+
+var edad = null;
+
+do {
+    if (edad !== null) {
+        alert("Por favor, ingrese un numero")
+    }
+    edad = prompt("Ingresá tu edad");
+} while (!Number(edad));
+
+
+if (Number(edad)) {
+    if (edad >= 10 && edad <= 14) {
+        alert("Edad entre 10 y 14")
+    } else if (edad > 14) {
+        alert("Edad mayor a 14")
+    } else if (edad < 10) {
+        alert("Edad menor a 10");
+    } else {
+        alert("ninguna opcion anterior")
+    }  
 }
